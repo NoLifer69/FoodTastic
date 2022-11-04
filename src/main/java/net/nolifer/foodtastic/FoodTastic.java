a0cb9bf545043f7729d3e9103a176a27533be465
Nolifer <loliamaoob@gmail.com>
2022-11-04 18:31:08 +0200

    added

--- src/main/java/net/nolifer/foodtastic/FoodTastic.java
+++ src/main/java/net/nolifer/foodtastic/FoodTastic.java
@@ -1,11 +1,13 @@
 package net.nolifer.foodtastic;
 
 import net.fabricmc.api.ModInitializer;
+import net.nolifer.foodtastic.item.ModItems;
 
 public class FoodTastic implements ModInitializer {
 	public static final String MOD_ID = "foodtastic";
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 	}
 }
--- src/main/java/net/nolifer/foodtastic/item/ModItems.java
+++ src/main/java/net/nolifer/foodtastic/item/ModItems.java
@@ -12,6 +12,19 @@ public class ModItems {
     public static final Item HAMBURGER = registerItem("hamburger",
             new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.HAMBURGER)));
 
+    public static final Item ORANGE = registerItem("orange",
+            new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.ORANGE)));
+
+    public static final Item BANANA = registerItem("banana",
+            new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.BANANA)));
+
+    public static final Item BLUE_GRAPE = registerItem("blue_grape",
+            new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.BLUE_GRAPE)));
+
+    public static final Item GREEN_GRAPE = registerItem("green_grape",
+            new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(ModFoodComponents.GREEN_GRAPE)));
+
+
     private static Item registerItem(String name, Item item){
         return Registry.register(Registry.ITEM, new Identifier(FoodTastic.MOD_ID, name), item);
     }
